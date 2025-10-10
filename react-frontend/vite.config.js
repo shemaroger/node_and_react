7e9b1445c6c72ca67535a3ea3fb9ab2aa7f4ece0
luckh-coder <shemaroger60@gmail.com>
2025-10-10 13:33:10 +0200

    update

--- react-frontend/vite.config.js
+++ react-frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ensure build output is in "dist"
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // This maps '@' to the 'src' folder
+    },
+  },
+});
